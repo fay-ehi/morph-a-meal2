5f2445a9894cad677abf351437c0cafbc18f0d93
fay-ehi <faithgoddey2002@gmail.com>
2024-12-12 01:23:47 +0100

    testing

--- netlify/functions/recipe-function.js
+++ netlify/functions/recipe-function.js
@@ -1,27 +1,34 @@
-import { HfInference } from "@huggingface/inference";
+// import { HfInference } from "@huggingface/inference";
+// exports.handler = async (event) => {
+//   const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+//   const SYSTEM_PROMPT = `
+// You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+// `;
 
-const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
-`;
+//   const ingredientsArr = JSON.parse(event.body).ingredients;
 
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+//   try {
+//     const ingredientsString = ingredientsArr.join(", ");
+//     const response = await hf.chatCompletion({
+//       model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+//       messages: [
+//         { role: "system", content: SYSTEM_PROMPT },
+//         {
+//           role: "user",
+//           content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+//         },
+//       ],
+//       max_tokens: 1024,
+//     });
 
-export async function getRecipeFromMistral(ingredientsArr) {
-  const ingredientsString = ingredientsArr.join(", ");
-  try {
-    const response = await hf.chatCompletion({
-      model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-      messages: [
-        { role: "system", content: SYSTEM_PROMPT },
-        {
-          role: "user",
-          content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
-        },
-      ],
-      max_tokens: 1024,
-    });
-    return response.choices[0].message.content;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ recipe: response.choices[0].message.content }),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: "An error occurred" }),
+//     };
+//   }
+// };
