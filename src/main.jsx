cf5a9f1d62dafb9c7f38e66ec512c7c5dc8e633f
fay-ehi <faithgoddey2002@gmail.com>
2024-12-12 01:46:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Ingredient from "./ingrdient";
+import Ingredient from "./ingredient";
 import Recipe from "./recipe";
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([
