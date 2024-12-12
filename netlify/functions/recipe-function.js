5186853ebec7046a7d0d48d51fa25c01dbc0f787
fay-ehi <faithgoddey2002@gmail.com>
2024-12-12 01:14:23 +0100

    Update recipe-function.js

--- netlify/functions/recipe-function.js
+++ netlify/functions/recipe-function.js
@@ -1,15 +1,14 @@
 import { HfInference } from "@huggingface/inference";
 
-exports.handler = async (event) => {
-  const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
-  const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page  
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 
-  const ingredientsArr = JSON.parse(event.body).ingredients;
+const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
 
+export async function getRecipeFromMistral(ingredientsArr) {
+  const ingredientsString = ingredientsArr.join(", ");
   try {
-    const ingredientsString = ingredientsArr.join(", ");
     const response = await hf.chatCompletion({
       model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
       messages: [
@@ -21,15 +20,8 @@ You are an assistant that receives a list of ingredients that a user has and sug
       ],
       max_tokens: 1024,
     });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ recipe: response.choices[0].message.content }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "An error occurred" }),
-    };
+    return response.choices[0].message.content;
+  } catch (err) {
+    console.error(err.message);
   }
-};
+}
