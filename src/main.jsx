c59b17a8cd9ba62101c65955da3db37a7c73d533
fay-ehi <faithgoddey2002@gmail.com>
2024-12-12 19:27:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,7 @@ import Ingredient from "./ingredient";
 import Recipe from "./recipe";
 import { getRecipeFromMistral } from "/netlify/functions/mistralRecipe.js";
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([
-    "spice",
-    "chicken",
-    "rice",
-    "carrots",
-  ]);
+  const [ingredients, setIngredients] = React.useState([]);
 
   const [recipe, setRecipe] = React.useState("");
 
